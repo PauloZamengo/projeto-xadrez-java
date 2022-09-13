6ac831836a87b1e9e3d5fa51a86fc63d473cd74b
PauloZamengo <paulinhozamengo@hotmail.com>
2022-09-12 22:17:56 -0300

    Criando lista de peças capturadas

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -7,7 +7,9 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -16,12 +18,13 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
 
         while (true) {
 
             try {
                 UI.clearScreen();
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.println("Origem: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -35,6 +38,11 @@ public class Program {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMovie(source, target);
+
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
+
             } catch (ChessException | InputMismatchException ex) {
                 System.out.println(ex.getMessage());
                 sc.nextLine();
--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -6,11 +6,17 @@ import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBord = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch() {
         board = new Board(8, 8);
@@ -66,8 +72,6 @@ public class ChessMatch {
         if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("Não existe movimento possível para a peça escolhida");
         }
-
-
     }
 
     private void validateTargetPosition(Position source, Position target) {
@@ -80,11 +84,17 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+
+        if (capturedPiece != null) {
+            piecesOnTheBord.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBord.add(piece);
     }
 
     private void nextTurn() {
